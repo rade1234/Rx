d4cd7e86e60cd03a3f200a9ca86747511beb02e8
rade1234 <qasdf180@wp.pl>
2018-01-13 16:55:41 +0100

    RxJS

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("działa na przeglądarce");
+import {Observable} from "rxjs";
+
+let numbers = [1, 5, 10];
+
+let source = Observable.create(observer => {
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout((produceValue, 250))
+        } else {
+            observer.complete();
+        }
+
+    }
+    produceValue();
+});
+
+source.subscribe(
+    value => console.log(`moja wartość to: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log(`skończył się stream`)
+);
---
+++ main.ts
@@ -0,0 +1,3 @@
+import { Observable } from "rxjs-es";
+
+let numbers = [1, 5, 10];
